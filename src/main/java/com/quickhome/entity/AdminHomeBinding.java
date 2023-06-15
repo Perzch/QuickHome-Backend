7850f759e19c60dd75896ef2e6f2825fd9720d0d
TimCScn <511055902@qq.com>
2023-06-15 19:57:54 +0800

    entity

---
+++ src/main/java/com/quickhome/entity/AdminHomeBinding.java
@@ -0,0 +1,32 @@
+package com.quickhome.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminHomeBinding {
+    private long bindingID_zch_hwz_gjc;
+    private long managerID_zch_hwz_gjc;
+    private long homeID_zch_hwz_gjc;
+    private String bindingType_zch_hwz_gjc;
+    private Date bindingTime_zch_hwz_gjc;
+    private String bindingState_zch_hwz_gjc;
+    private String standby1_zch_hwz_gjc;
+    private String standby2_zch_hwz_gjc;
+    private String standby3_zch_hwz_gjc;
+    private int visible_zch_hwz_gjc;
+
+    public AdminHomeBinding(long bindingID_zch_hwz_gjc, long managerID_zch_hwz_gjc, long homeID_zch_hwz_gjc, String bindingType_zch_hwz_gjc, Date bindingTime_zch_hwz_gjc, String bindingState_zch_hwz_gjc) {
+        this.bindingID_zch_hwz_gjc = bindingID_zch_hwz_gjc;
+        this.managerID_zch_hwz_gjc = managerID_zch_hwz_gjc;
+        this.homeID_zch_hwz_gjc = homeID_zch_hwz_gjc;
+        this.bindingType_zch_hwz_gjc = bindingType_zch_hwz_gjc;
+        this.bindingTime_zch_hwz_gjc = bindingTime_zch_hwz_gjc;
+        this.bindingState_zch_hwz_gjc = bindingState_zch_hwz_gjc;
+    }
+}
