ca7c61723b1a4c6776f14ad2cac7821af5b49ce6
TimCScn <511055902@qq.com>
2023-06-18 23:51:25 +0800

    修正标识符

--- src/main/java/com/quickhome/util/CreatAccount.java
+++ src/main/java/com/quickhome/util/CreatAccount.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public final class CreatAccount {
     public static long creatAccount() {
-        int min_zch_hwz_gjc = 1000000;
-        int max_zch_hwz_gjc = 99999999;
+        int minAccount = 1000000;
+        int maxAccount = 99999999;
         Random random_zch_hwz_gjc = new Random();
-        return random_zch_hwz_gjc.nextInt(max_zch_hwz_gjc - min_zch_hwz_gjc + 1) + min_zch_hwz_gjc;
+        return random_zch_hwz_gjc.nextInt(maxAccount - minAccount + 1) + minAccount;
     }
 }
--- src/main/java/com/quickhome/util/DynamicDoorPassword.java
+++ src/main/java/com/quickhome/util/DynamicDoorPassword.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public final class DynamicDoorPassword {
     public static Integer dynamicDoorPassword(){
-        int min_zch_hwz_gjc = 100000;
-        int max_zch_hwz_gjc = 999999;
+        int minDynamicDoorPassword = 100000;
+        int maxDynamicDoorPassword = 999999;
         Random random_zch_hwz_gjc = new Random();
-        return random_zch_hwz_gjc.nextInt(max_zch_hwz_gjc - min_zch_hwz_gjc + 1) + min_zch_hwz_gjc;
+        return random_zch_hwz_gjc.nextInt(maxDynamicDoorPassword - minDynamicDoorPassword + 1) + minDynamicDoorPassword;
     }
 }
